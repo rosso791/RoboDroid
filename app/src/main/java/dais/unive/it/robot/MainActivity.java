e0940d940cf48ee0a798be44ba46ce49c6a39fa3
Federico Mariano <rosso791@libero.it>
2018-12-07 10:41:03 +0100

    initial commit

---
+++ app/src/main/java/dais/unive/it/robot/MainActivity.java
@@ -0,0 +1,13 @@
+package dais.unive.it.robot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
