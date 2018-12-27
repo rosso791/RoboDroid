59f4b7e3fcf329378a8b188fbca396729b8e14af
Federico Mariano <rosso791@libero.it>
2018-12-27 23:20:56 +0100

    Add files via upload

--- app/src/main/java/dais/unive/it/robot/ActivityApp/MainActivity.java
+++ app/src/main/java/dais/unive/it/robot/ActivityApp/MainActivity.java
@@ -1,10 +1,14 @@
 package dais.unive.it.robot.ActivityApp;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -17,10 +21,25 @@ import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main Activity";
     private static int TIME_OUT = 1000;
+    private Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Start button that leads to MenuActivity
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+            }
+        });
+        //hello
+
+        //TODO da attivare
+        /*
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         try{
             EV3 ev3 = new EV3(new BluetoothConnection("EV3").connect());
             new Handler().postDelayed(() -> {
@@ -33,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "fatal error: cannot connect to EV3");
             e.printStackTrace();
         }
+        */
 
     }
 }
