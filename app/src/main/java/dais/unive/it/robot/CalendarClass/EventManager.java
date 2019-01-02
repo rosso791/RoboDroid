98128c0ab693ba375b2d798fd8a6e72753267ca5
marcopanino <marco@softml.it>
2019-01-02 16:23:00 +0100

    Added method to get a list of events in a hashmap

--- app/src/main/java/dais/unive/it/robot/CalendarClass/EventManager.java
+++ app/src/main/java/dais/unive/it/robot/CalendarClass/EventManager.java
@@ -5,21 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import dais.unive.it.robot.Automation.DataExchange;
+
 public class EventManager extends AppCompatActivity {
 
+    private static final String FILENAME = "/calendar.json";
     private static EventManager instance;
 
     Timer timer = new Timer();
@@ -27,20 +34,19 @@ public class EventManager extends AppCompatActivity {
     private List<Event> events;
 
     private EventManager() {
-        events = EventManager.deserialize();
-//        timer.schedule( new TimerTask() {
-//            public void run() {
-//
-//                // ToDo aggiungere il metodo per rilasciare le pillole dentro il foreach
-////                events
-////                        .stream()
-////                        .filter(e -> e.getWhen().get(Calendar.HOUR) == Calendar.getInstance().get(Calendar.HOUR)
-////                            && e.getWhen().get(Calendar.MINUTE) == Calendar.getInstance().get(Calendar.MINUTE)
-////                            && e.getDay().ordinal() == Calendar.getInstance().get(Calendar.DAY_OF_WEEK))
-////                        .forEach(e -> e.);
-//
-//            }
-//        }, 0, 10*1000);
+        events = EventManager.deserialize(Environment.getExternalStorageDirectory() + FILENAME);
+        timer.schedule( new TimerTask() {
+            public void run() {
+                DataExchange dataExchange = DataExchange.GetInstance();
+                    events
+                    .stream()
+                    .filter(e -> e.getWhen().get(Calendar.HOUR) == Calendar.getInstance().get(Calendar.HOUR)
+                        && e.getWhen().get(Calendar.MINUTE) == Calendar.getInstance().get(Calendar.MINUTE)
+                        && e.getDay().ordinal() == Calendar.getInstance().get(Calendar.DAY_OF_WEEK))
+                    .findFirst()
+                    .ifPresent(x -> DataExchange.SetColorDischargeRequest(x.getColorCode()));
+            }
+        }, 0, 10*1000);
     }
 
     public static EventManager GetInstance() {
@@ -50,26 +56,29 @@ public class EventManager extends AppCompatActivity {
         return instance;
     }
 
-    private static List<Event> deserialize() {
+    private static List<Event> deserialize(String path) {
         List<Event> events = new ArrayList<>();
         Gson unserializer = new Gson();
         try {
-            File file = new File(Environment.getDataDirectory() + "/calendar.json");
+            File file = new File(path);
             if(file.exists()) {
-                FileReader r = new FileReader(Environment.getDataDirectory() + "/calendar.json");
-                instance.events = unserializer.fromJson(r, List.class);
+                FileReader r = new FileReader(path);
+                Type t = new TypeToken<List<Event>>(){}.getType();
+                events = (ArrayList<Event>)unserializer.fromJson(r, t);
             }
         } catch (IOException e) {
             Toast.makeText(instance.getApplicationContext(), "Error reading a backup of the calendar", Toast.LENGTH_LONG).show();
         }
         return events;
     }
-    private static void serialize() {
+    private static void serialize(String path) {
         Gson serializer = new Gson();
         try {
-            File file = new File(Environment.getDataDirectory() + "/calendar.json");
+            File file = new File(path);
             FileWriter w = new FileWriter(file);
             serializer.toJson(instance.events, w);
+            w.flush();
+            w.close();
         } catch (IOException e) {
             Toast.makeText(instance.getApplicationContext(), "Error writing a backup of the calendar", Toast.LENGTH_LONG).show();
         }
@@ -91,11 +100,23 @@ public class EventManager extends AppCompatActivity {
                 events.add(new Event(day, color, when, false));
                 break;
         }
-        serialize();
+        serialize(Environment.getExternalStorageDirectory() + FILENAME);
     }
 
     public void DeleteEvent(Event e) {
         events.remove(e);
-        serialize();
+        serialize(Environment.getExternalStorageDirectory() + FILENAME);
     }
+
+    public Map<Integer, Map<WeekDay, Event>> getAllEvents() {
+        Map<Integer, Map<WeekDay, Event>> eventsList = new HashMap<>();
+        events
+                .forEach(e -> {
+                    int when = e.getWhen().get(Calendar.HOUR) * 60 + e.getWhen().get(Calendar.MINUTE);
+                    if(!eventsList.containsKey(when)) eventsList.put(when, new HashMap<>());
+                    eventsList.get(when).put(e.getDay(), e);
+                });
+        return eventsList;
+    }
+
 }
